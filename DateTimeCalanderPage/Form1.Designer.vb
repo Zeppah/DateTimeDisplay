f1944aaf64c3a04ad3170af1caabb6cab56ce782
Larry <larry.benner@gmail.com>
2020-06-06 07:33:54 -0400

    Add project files.

---
+++ DateTimeCalanderPage/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.LblYear = New System.Windows.Forms.Label()
+        Me.LblMonth = New System.Windows.Forms.Label()
+        Me.LblDay = New System.Windows.Forms.Label()
+        Me.LblTime = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'LblYear
+        '
+        Me.LblYear.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LblYear.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.LblYear.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblYear.Location = New System.Drawing.Point(0, 0)
+        Me.LblYear.Name = "LblYear"
+        Me.LblYear.Size = New System.Drawing.Size(411, 91)
+        Me.LblYear.TabIndex = 0
+        Me.LblYear.Text = "1955"
+        Me.LblYear.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LblMonth
+        '
+        Me.LblMonth.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LblMonth.BackColor = System.Drawing.Color.Aqua
+        Me.LblMonth.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblMonth.Location = New System.Drawing.Point(2, 91)
+        Me.LblMonth.Name = "LblMonth"
+        Me.LblMonth.Size = New System.Drawing.Size(409, 91)
+        Me.LblMonth.TabIndex = 1
+        Me.LblMonth.Text = "May"
+        Me.LblMonth.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LblDay
+        '
+        Me.LblDay.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LblDay.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.LblDay.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblDay.Location = New System.Drawing.Point(2, 182)
+        Me.LblDay.Name = "LblDay"
+        Me.LblDay.Size = New System.Drawing.Size(409, 91)
+        Me.LblDay.TabIndex = 2
+        Me.LblDay.Text = "15"
+        Me.LblDay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LblTime
+        '
+        Me.LblTime.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LblTime.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.LblTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblTime.Location = New System.Drawing.Point(2, 273)
+        Me.LblTime.Name = "LblTime"
+        Me.LblTime.Size = New System.Drawing.Size(409, 91)
+        Me.LblTime.TabIndex = 3
+        Me.LblTime.Text = "05:00:00"
+        Me.LblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(412, 364)
+        Me.Controls.Add(Me.LblTime)
+        Me.Controls.Add(Me.LblDay)
+        Me.Controls.Add(Me.LblMonth)
+        Me.Controls.Add(Me.LblYear)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents LblYear As Label
+    Friend WithEvents LblMonth As Label
+    Friend WithEvents LblDay As Label
+    Friend WithEvents LblTime As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ DateTimeCalanderPage/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim today As Date = Date.Now
+
+        LblYear.Text = Format(today, "yyyy")
+        LblMonth.Text = Format(today, "MMM")
+        LblDay.Text = Format(today, "dd")
+        LblTime.Text = Format("hh:mm:ss")
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim temp As Date = Date.Now
+        LblTime.Text = Format(temp, "hh:mm:ss")
+        Text = Format(temp, "D")
+    End Sub
+
+End Class
